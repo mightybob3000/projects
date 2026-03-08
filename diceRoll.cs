d89391c5d0da26483d4bac7fe419f93d7de62501
mightybob3000 <115615740+mightybob3000@users.noreply.github.com>
2026-03-07 23:47:26 -0800

    Add files via upload

---
+++ diceRoll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+File.Delete("diceRolls.txt");
+Console.WriteLine("How big is the dice? (Enter a number)");
+int diceSize = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many times do you want to roll the dice? (Enter a number)");
+int rolls = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < rolls; i++)
+{
+Random random = new Random();
+int diceRoll = random.Next(1, diceSize + 1);
+//Console.Write(" ," + diceRoll);
+using (FileStream fs = new FileStream("diceRolls.txt", FileMode.Append, FileAccess.Write))
+using (StreamWriter sw = new StreamWriter("diceRolls.txt", true))
+    {
+        sw.WriteLine(diceRoll);
+    }
+}
+Console.ReadLine();
